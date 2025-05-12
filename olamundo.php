d32194eff6c677df41e8d8ade2df454e5fff0be1
luccakeidann <04101468036@senacrs.edu.br>
2025-05-12 12:23:28 -0300

    Add files via upload

---
+++ olamundo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<style>
+
+h1 {
+
+font-family: arial;
+font-size: 50px;
+text-align: center;
+color: red;
+}
+</style>
+<body>
+
+<?php
+echo "<h1>Primeiro Programa</h1>";
+echo "<h2> Segundo Ano</h2>";
+echo "<h3> Senac </h3>";
+echo "Olá Mundo";
+?> 
+
+</body>
+</html>

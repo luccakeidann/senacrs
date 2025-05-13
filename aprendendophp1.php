1512269a131bdaa67b420227a6bb6e43e47ed8e4
luccakeidann <04101468036@senacrs.edu.br>
2025-05-13 11:42:45 -0300

    Add files via upload

---
+++ aprendendophp1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 2025;
+echo var_dump($x);
+$nome = "SenacCaxiasDoSul";
+echo "<br>";
+echo var_dump($nome);
+$salario= 1890.99;
+echo "<br>";
+echo var_dump($salario);
+$ehestudante = true;
+echo "<br>";
+echo var_dump($ehestudante);
+
+?> 
+
+</body>
+</html>

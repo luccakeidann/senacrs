9bfbb088f18447fc1ec90a77591a8bc84c040915
luccakeidann <04101468036@senacrs.edu.br>
2025-05-13 12:13:21 -0300

    Add files via upload

---
+++ calculadora.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$a = 10;
+$b = 5;
+$soma = $a + $b;
+echo "<h2> Soma </h2>";
+echo "$a + $b = $soma";
+$sub = $a - $b;
+echo "<h2> Subtração </h2>";
+echo "$a - $b = $sub";
+$mult = $a * $b;
+echo "<h2> Multiplicação </h2>";
+echo "$a * $b = $mult";
+$div = $a/$b;
+echo "<h2> Divisão </h2>";
+echo " $a/$b=$div";
+$resto = $a % $b;
+echo "<h2> Resto </h2>";
+echo "$a % $b=$resto";
+
+?> 
+
+</body>
+</html>

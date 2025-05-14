ec741afdbb9e0ecb5a4a5920188b2fb207ab65f7
luccakeidann <04101468036@senacrs.edu.br>
2025-05-14 10:05:26 -0300

    Add files via upload

---
+++ entrevista.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<style>
+
+h1 {
+
+font-family: arial;
+font-size: 20px;
+text-align: center;
+color: grey;
+}
+</style>
+<body>
+
+<?php
+echo "<h1>Projeto Integrador</h1>";
+$nome = "Barry";
+echo "<h2> Entrevista </h2>";
+echo "<br>";
+echo " Diretor: Olá $nome, Como vai?";
+echo "<br>";
+echo " $nome: Tudo Bem e você?";
+echo "<br>";
+echo " Diretor: Estou ótimo, queria perguntar como está seus estudos aqui na escola, é como se fosse uma entrevista, quero saber se têm algo que você tem dificuldade, estamos aqui para te ajudar, de acordo?";
+echo "<br>";
+echo " $nome: 100% de acordo, pode perguntar.";
+echo "<br>";
+echo " Diretor: Muito bem, primeiramente, como funciona o esquema de estudos para você?";
+echo "<br>";
+echo " $nome: Bom, como você já deve saber, eu sou daltônico, então às vezes isso me atrapalha.";
+echo "<br>";
+echo " Diretor: Hum, e algum professor já fez alguma adaptação para você ?";
+echo "<br>";
+echo " $nome: Sim, a prof de artes troca as cores por figuras, e cada figura representa uma cor, aí consigo entender ?";
+echo "<br>";
+echo " Diretor: Legal, que ideia bacana, vou pedir para os outros professores fazerem isso também. E as outras coisas, escrever, desenhar, tudo certo?";
+echo "<br>";
+echo "$nome: Sim, em geral tudo ótimo.";
+echo "<br>";
+echo "Diretor: então tá, fico feliz que você está se sentindo bem aqui e só vou pedir aos outros professores aquela adaptação que te ajuda muito, era isso, obrigado pela cooperação Barry.";
+echo "<br>";
+echo "$nome: Não há de que diretor.";
+
+
+
+
+?> 
+
+</body>
+</html>

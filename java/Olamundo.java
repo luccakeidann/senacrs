3e04006b2fdb7f83bc0be066d7d025edd630168b
luccakeidann <04101468036@senacrs.edu.br>
2025-08-06 10:27:24 -0300

    Add files via upload

---
+++ java/Olamundo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olamundo;
+
+/**
+ *
+ * @author LUCCAKEIDANNOLIVEIRA
+ */
+public class Olamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Método Principal
+        String nome = "Lucca";
+        String sobrenome = "Keidann";
+        System.out.println("Ola Mundo!");
+        System.out.println("Bom dia "+nome);
+        System.out.println(nome+" "+sobrenome);
+    }
+    
+}
